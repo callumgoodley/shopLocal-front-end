cee5796b69a1d94cbe2bbc83afabbb2715ea9627
Mohamed Fraitis <mohamedfraitis@Mohameds-MacBook-Air.local>
2020-02-19 15:45:21 +0000

    bottom nav

--- App.js
+++ App.js
@@ -6,18 +6,71 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomePage from './Screens/HomePage';
 import LoginPage from './Screens/LoginPage';
+import MyAccountPage from './Screens/MyAccountPage';
+import SettingsPage from './Screens/SettingsPage';
+// import {Ionicons} from 'react-native-vector-icons/Ionicons ';
 
-const Stack = createStackNavigator();
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-function App() {
+// const Stack = createStackNavigator();
+
+// function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="Home">
+//         {/* <Stack.Screen name="Login" component={LoginPage} /> */}
+//         <Stack.Screen name="Home" component={HomePage} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+const HomeStack = createStackNavigator();
+
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomePage} />
+    </HomeStack.Navigator>
+  );
+}
+
+const SettingsStack = createStackNavigator();
+
+function SettingsStackScreen() {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Settings" component={SettingsPage} />
+    </SettingsStack.Navigator>
+  );
+}
+
+const MyAccountStack = createStackNavigator();
+
+function MyAccountStackScreen() {
+  return (
+    <MyAccountStack.Navigator>
+      <MyAccountStack.Screen
+        name="My Account"
+        component={MyAccountPage}
+        options={{title: 'Hi'}}
+      />
+    </MyAccountStack.Navigator>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginPage} />
-        <Stack.Screen name="Home" component={HomePage} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="My Account" component={MyAccountStackScreen} />
+        <Tab.Screen name="Settings" component={SettingsStackScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
 
-export default App;
+// export default App;
---
+++ Screens/MyAccountPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+function MyAccountPage() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>My Account!</Text>
+    </View>
+  );
+}
+
+export default MyAccountPage;
