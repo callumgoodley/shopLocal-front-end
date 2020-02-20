323738b95fb250ada06416a032110e23d5af7f19
Mohamed Fraitis <mohamedfraitis@mohameds-air.home>
2020-02-20 19:28:54 +0000

    refactor image style

--- Components/ShopCard.js
+++ Components/ShopCard.js
@@ -4,7 +4,7 @@ import {Text, View, Image} from 'react-native';
 const ShopCard = () => {
   return (
     <View style={styled.card}>
-      <View style={{flex: 1}}>
+      <View style={styled.imageView}>
         <Image
           source={require('../images/bakeryPic.jpg')}
           style={styled.image}
@@ -22,10 +22,11 @@ const styled = {
     flex: 1,
     backgroundColor: 'rgba(20, 156, 12, 0.3)',
     borderRadius: 20,
-    height: 250,
+    height: 200,
     marginTop: 20,
   },
   text: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+  imageView: {flex: 1},
   image: {
     flex: 1,
     width: null,
